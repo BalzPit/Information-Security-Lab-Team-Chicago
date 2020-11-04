d2ec89182025d04cc8658c5dba16be75da428f3c
Christian <christiancattai@gmail.com>
2020-11-04 22:12:06 +0100

    Fixed Analyzer.m
    Now we have the right way to use multiplication in binary set

--- analyzer.m
+++ analyzer.m
@@ -3,8 +3,11 @@ A = A(:,1:32)';
 B = readmatrix("linear_text_output.txt");
 B = B(:,1:32)';
 
-text = hexToBinaryVector('80000000', 32)';
-key = hexToBinaryVector('80000000', 32)';
+text = double(hexToBinaryVector('12345678', 32)');
+key = double(hexToBinaryVector('87654321', 32)');
 
-result = (xor(A*key, B*text))
+A_k = mod(A*key,2);
+B_u = mod(B*text,2);
+
+result = xor(A_k,B_u);
 Hex = binaryVectorToHex(result')
